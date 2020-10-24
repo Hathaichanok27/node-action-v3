a6434908c4114de00d63c98e995028795af2f43c
Hathaichanok27 <60160008@go.buu.ac.th>
2020-10-24 16:01:07 +0700

    Add API GET '/items'

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const items = [
+    { id: 1, name:'iPhone12 Pro Max'},
+    { id: 2, name:'Google Pixel 5'}
+]
 
 app.get('/', (req, res) => {
-    return res.send('Hello')
+    return res.send('Hello!')
 })
 
 app.listen(port, () => {
     console.log(`Start Express application at ${port}`)
 })
 
+app.get('/items', (req, res) => {
+    return res.json(items)
+})
 module.exports = app
